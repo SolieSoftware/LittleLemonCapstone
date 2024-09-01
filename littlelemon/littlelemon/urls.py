6f6c47e54fb84490c2bff4a5f0b33a31b298533e
solsh <sol.shortland@gmail.com>
2024-09-01 20:08:55 +0100

    Update dommits

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from restaurant import views
+
+
+router = DefaultRouter()
     
 router.register(r'tables', views.BookingViewSet)
 
@@ -25,5 +29,6 @@ urlpatterns = [
     path('restaurant/', include('restaurant.urls')),
     path('restaurant/menu/',include('restaurant.urls')),
     path('restaurant/booking/', include(router.urls)),
-
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes, api_view
 from .models import Booking, Menu
 from .serializers import BookingSerializer, MenuSerializer
 
 # Create your views here.
+
 def index(request):
     return render(request, 'index.html', {})
 
-class MenuItem(ListCreateView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
+class MenuItem(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
 
 
 class SingleMenuItem(RetrieveUpdateAPIView, DestroyAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
 
 class BookingViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
