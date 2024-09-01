f0c990c0143e888b7e8bc4ad3a99f6158fb4ae99
solsh <sol.shortland@gmail.com>
2024-09-01 17:26:32 +0100

    Reg commit

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItem(ListCreateView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class SingleMenuItem(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
